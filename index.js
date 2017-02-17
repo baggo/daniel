85c5bbdf4a5326af77ff3108caa035dc570465cd
Dustin Dowell <dustindowell22@gmail.com>
2017-02-16 19:38:19 -0600

    Commit project

---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+console.log('Started!')
+
+const Client = require('discord.js').Client
+const bot = new Client()
+const spawn = require('child_process').spawn
+
+let q = []
+let busy = false
+
+// let acros = {
+//   'lol': 'licking old lips',
+//   'lmao': 'laughing my ass off',
+//   'lmfao': 'laughing my fucking ass off',
+//   'imo': 'in my opinion',
+//   'rip': 'rest in peace'
+// }
+
+// for (var acro in acros) {
+//   message = name + ' says ' + content.replace(new RegExp('\s*' + acro + '\s*'), acros[acro])
+// }
+
+var name_previous = ''
+
+bot.on('message', msg => {
+  var name = msg.member.displayName
+  var content = msg.content
+  var message
+
+  if (content === '') {
+    message = name + ' uploaded media'
+  } else if (name === name_previous) {
+    message = content
+  } else {
+    message = name + ' says ' + content
+  }
+
+  name_previous = name
+
+  if (q.length < 10) q.push(message)
+  if (!busy) say(q.shift())
+})
+
+function say(message) {
+  busy = true
+  var proc = spawn('say', [message])
+  proc.on('exit', () => {
+    busy = false
+    if (q.length) say(q.shift())
+  })
+}
+
+bot.login(require('./token'))
